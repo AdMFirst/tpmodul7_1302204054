da4039bcc0f70ac6bda5c83ec44e2201c002baa8
Aditya MP <211wadityaw112@gmail.com>
2022-04-08 13:49:13 +0700

    menyelesaikan program dengan runtime configuration

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
 ﻿
+namespace com.tpmodul7_1302204054
+{
+
+    public class Program 
+    { 
+
+        public static void Main()
+        {
+            CovidConfig config = new CovidConfig();
+            config.ReadJSON();
+
+            Console.WriteLine($"Berapakah suhu badan anda saat ini ? Dalam nilai {config.CONFIG1}");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman ?");
+            int input2 = Convert.ToInt32( Console.ReadLine());
+
+            bool kondisi1;
+            if(config.CONFIG1 == "celcius") {
+                kondisi1 = (input1 >= 36.5) && (input1 <= 37.5);
+            } else {
+                kondisi1 = (input1 >= 97.7) && (input1 <= 99.5);
+            }
+
+            if (kondisi1 && input2 < config.CONFIG2)
+            {
+                Console.WriteLine(config.CONFIG4);
+            } else
+            {
+                Console.WriteLine(config.CONFIG3);
+            }
+
+            Console.WriteLine("\nTest ubah config1");
+            Console.WriteLine(config.CONFIG1);
+            config.UbahSatuan();
+            Console.WriteLine(config.CONFIG1);
+        }
+    }
+}
